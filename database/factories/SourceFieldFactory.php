361bf41282aa3eb59038895635161a6e4b3f8d59
samuchiri <s.muchiri.999@gmail.com>
2021-05-29 20:12:41 +0300

    updated

---
+++ database/factories/SourceFieldFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SourceField;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SourceFieldFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = SourceField::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
