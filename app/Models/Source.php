b3e063655753c51f76837831b903b9f1493fdc2d
samuchiri <s.muchiri.999@gmail.com>
2021-05-30 17:09:35 +0300

    updated

--- app/Models/Source.php
+++ app/Models/Source.php
@@ -8,10 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Source extends Model
 {
     use HasFactory;
-
-
+    protected $table='sources';
+    protected $fillable=['name','bio','status','title'];
 
     public function contact(){
     	return $this->haMany('App\Models\Contact','contact_id','id');
+    }
+    public function source_field(){
+    	return $this->hasMany('App\Models\SourceField','source_field_id','id');
     }
 }
--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Subscription extends Model
 {
     use HasFactory;
+    protected $table='subscriptions';
+    protected $fillable=['user_id','package_id','date','expiry_date','status','amount'];
+
+    public function user(){
+    	return $this->hasOne('App\Models\User','id','user_id');
+    }
+    public function package(){
+    	return $this->hasOne('App\Models\Package','id','package_id');
+    }
 }
